524a27b678124c0d6736739b57353551acdaef56
Javharbek <javhar_work@mail.ru>
2018-05-02 16:27:38 +0500

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+VK Social Sharer
+=========
+Vk Social Sharer
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer require --prefer-dist jakharbek/vk-sharer "dev-master"
+```
+
+or add
+
+```
+"jakharbek/vk-sharer": "dev-master"
+```
+
+to the require section of your `composer.json` file.
---
+++ src/VK.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace jakharbek\vksharer;
+use \BW\Vkontakte;
+
+class VK
+{
+    /**
+     * @var $client_id
+     * для груп с отрицательным значением
+     */
+    public $client_id;
+    /**
+     * @var $app_id идентификатор приложение
+     */
+    public $app_id;
+    /**
+     * @var $secret_code Защищённый ключ
+     */
+    public $secret_code;
+    /**
+     * @var $vk Vkontakte
+     */
+    public $vk;
+    /**
+     * @var string $redirect_uri
+     */
+    public $redirect_uri = "https://oauth.vk.com/blank.html";
+    /**
+     * @var $login_url самоопределаюищий
+     */
+    public $login_url;
+    public $response_type = "token";
+    /**
+     * @var array $premissions scopes
+     */
+    public $premissions = [
+        'notify',
+        'friends',
+        'photos',
+        'audio',
+        'video',
+        'stories',
+        'pages',
+        'status',
+        'notes',
+        'messages',
+        'wall',
+        'ads',
+        'offline',
+        'docs',
+        'groups',
+        'notifications',
+        'stats',
+        'email',
+        'market'
+    ];
+
+    /**
+     * init
+     */
+    public function init(){
+        $vk = new Vkontakte([]);
+        $vk->setSecret($this->secret_code);
+        $vk->setAppId($this->app_id);
+        $vk->setRedirectUri($this->redirect_uri);
+        $vk->setResponceType($this->response_type);
+        $vk->setScope($this->premissions);
+        $this->login_url = $vk->getLoginUrl();
+        $this->connection();
+    }
+
+    /**
+     * @return string $token;
+     */
+    public function getToken(){
+        $file = "vk.token";
+        if(!file_exists($file)){
+            $this->setToken("");
+        }
+        return file_get_contents($file);
+    }
+
+    /**
+     * @param $token
+     * @return bool|int
+     */
+    public function setToken($token){
+        $file = "vk.token";
+        return file_put_contents($file,$token);
+    }
+
+    /**
+     * connection in api
+     */
+    public function connection(){
+        $token = $this->getToken();
+        $this->vk = new Vkontakte(['access_token' => $token]);
+    }
+
+    /**
+     * @param $message
+     * @param $link
+     * @return mixed
+     */
+    public function post($message,$link){
+        return $this->vk->api('wall.post',[
+            'owner_id' => $this->client_id,
+            'message' => $message,
+            'attachments' => $link
+        ]);
+    }
+}
